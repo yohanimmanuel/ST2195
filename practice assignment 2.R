75e25df3083f7d4a175fa9ce23793c6332dff928
yohanimmanuel <101618425+yohanimmanuel@users.noreply.github.com>
2025-11-03 22:09:52 +0800

    22:09 03/11/2025

--- practice assignment 2.R
+++ practice assignment 2.R
@@ -7,10 +7,16 @@ csv_wiki <- read_html(url)
 xpath_string = "//*[@id=mw-content-text]/div[1]/pre"
 csv_tables <- csv_wiki %>%
   html_nodes(xpath = xpath_string)
-  html_text()
+
+html_text()
 csv_tables
 
 csv_table <- csv_tables[1]
 
-write.table(csv_table, file="")
+write.table(csv_table, file="data.csv")
 
+quote = FALSE
+col.names = FALSE
+row.names = FALSE
+
+read.csv("data.csv")  
